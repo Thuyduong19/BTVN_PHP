c374b47d83b84eec65a02a4208510992f4a0e34f
Thuy duong <thuyduong1910204@gail.com>
2024-08-16 14:15:38 +0700

    add file.php

---
+++ file.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<style>
+.file img{
+    height:350px;
+    width:350px;
+}
+.file a h2:hover{
+    color: black;
+}
+
+</style>
+    <?php
+
+    echo'<div class="file" >
+    <h1 style="text-align:center;color:black;">Tài liệu speaking part 1</h1>
+    <table cellspacing="20" style="margin-left:30px;">
+        <tr>
+            <td><img src="dream.png"></td>
+            <td><img src="hair.png"></td>
+            <td><img src="concentration.png"></td>
+        </tr>
+        <tr>
+        <td style="text-align:center;"><a style="text-decoration:none;color:gray;" target="_blank" href="https://ielts-nguyenhuyen.com/bai-mau-ielts-speaking-part-1-topic-dreams-giac-mo/"><h2>Topic:Dream</h2></a></td>
+        <td style="text-align:center;"><a style="text-decoration:none;color:gray;" target="_blank" href="https://ielts-nguyenhuyen.com/bai-mau-ielts-speaking-part-1-chu-de-hairstyle/"><h2>Topic:Hairstyles</h2></a></td>
+        <td style="text-align:center;"><a style="text-decoration:none;color:gray;" target="_blank" href="https://ielts-nguyenhuyen.com/ielts-speaking-part-1-concentration/"><h2>Topic:Concentration</h2></a></td>
+        </tr>
+         <tr>
+            <td><img src="dream.png"></td>
+            <td><img src="hair.png"></td>
+            <td><img src="concentration.png"></td>
+        </tr>
+        <tr>
+        <td style="text-align:center;"><a style="text-decoration:none;color:gray;" target="_blank" href="https://ielts-nguyenhuyen.com/bai-mau-ielts-speaking-part-1-topic-dreams-giac-mo/"><h2>Topic:Family</h2></a></td>
+        <td style="text-align:center;"><a style="text-decoration:none;color:gray;" target="_blank" href="https://ielts-nguyenhuyen.com/bai-mau-ielts-speaking-part-1-chu-de-hairstyle/"><h2>Topic:Home</h2></a></td>
+        <td style="text-align:center;"><a style="text-decoration:none;color:gray;" target="_blank" href="https://ielts-nguyenhuyen.com/ielts-speaking-part-1-concentration/"><h2>Topic:Plant</h2></a></td>
+        </tr>
+    </table></div>'
+    ?>
+</body>
+</html>
